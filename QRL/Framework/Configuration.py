14cd4d232e19afa404fcc0adf1512311f5138a18
l45k <l45k@users.noreply.github.com>
2020-03-09 11:03:10 +0100

    Initial commit for the quantum reinforcement learning framework.

---
+++ QRL/Framework/Configuration.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2018, 2019.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+
+class Configuration:
+    def __init__(self, nb_iterations, training_params, cooling_scheme, batch_size, plot_training=False, average=100):
+        self.nb_iterations = nb_iterations
+        self.training_params = training_params
+        self.cooling_scheme = cooling_scheme
+        self.batch_size = batch_size
+        self.plot_training = plot_training
+        self.average = average
