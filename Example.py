d47b91e3c469233376faa85fa7d192044c45dc1e
l45k <l45k@users.noreply.github.com>
2020-03-10 15:35:05 +0100

    Example for the VQDQL and Qlearner.

--- Example.py
+++ Example.py
@@ -13,10 +13,16 @@
 # that they have been altered from the originals.
 
 from Agents.VQDQL import VQDQL
+from Agents.Qlearner import Qlearner
+from Agents.DQN import DQN
 from Framework.Configuration import Configuration
 from Framework.Evaluator import Evaluator
+from Framework.utils import get_num_states
 import gym
 import Environments.FL
+from keras.models import Sequential
+from keras.layers import Dense, Activation
+from keras.optimizers import Adam
 
 # Setup the environment containing
 env = gym.make('FL-v0')
@@ -30,16 +36,38 @@ training_params =[.1, .99, .9]
 cooling_schemes = [lambda x, iter: x, lambda x, iter: x, lambda x,iter: x*(0.99**iter)]
 
 # Instantiate the trainer, conta
-conf = Configuration(nb_iterations=nb_iterations, training_params=training_params, cooling_scheme=cooling_schemes,
+confVQD = Configuration(nb_iterations=nb_iterations, training_params=training_params, cooling_scheme=cooling_schemes,
                         batch_size=batch_size, plot_training=True, average=int(batch_size/5))
+
+confQ = Configuration(nb_iterations=10000, training_params=training_params, cooling_scheme=cooling_schemes,
+                        batch_size=1, plot_training=True, average=int(batch_size/100))
+
+# NN model for DQN
+num_states = get_num_states(env)
+model = Sequential()
+model.add(Dense(num_states, input_shape=(16, )))
+model.add(Activation('relu'))
+model.add(Dense(num_states))
+model.add(Activation('relu'))
+model.add(Dense(env.action_space.n))
+model.add(Activation('linear'))
+print(model.summary())
+
+confDQN = Configuration(nb_iterations=1000, training_params=training_params[1:], cooling_scheme=cooling_schemes[1:], batch_size=30, plot_training=True,memory_size=500,
+                        average=int(batch_size/100))
+confDQN.model = model
+confDQN.target_replacement = 1e1
 
 # Example to train two VQDQL agents
 
-agent = VQDQL(env, memory_size= 100, nb_variational_circuits=1, configuration=conf)
-#agent1 = VQDQL(env, memory_size= 50, nb_variational_circuits=1, debug=True, configuration=conf)
+agent = VQDQL(env, memory_size= 100, nb_variational_circuits=1, configuration=confVQD)
+agent1 = Qlearner(env, debug=True, configuration=confQ)
+#agent2 = DQN(env, debug=True, configuration=confDQN)
+
+#agent2.evaluate(100)
 
 # Compare the performance
-evaluator = Evaluator([agent, agent], 5)
+evaluator = Evaluator([agent, agent1], 5)
 rewards = evaluator.evaluate(plot_results=True, average=2)
 
 
