5de3f0cdb52a938dcf5c3e713af887f1742feb8e
l45k <l45k@users.noreply.github.com>
2020-11-04 13:13:00 +0100

    Clean for merge

--- Agents/DQN.py
+++ Agents/DQN.py
@@ -15,33 +15,31 @@
 from Framework.utils import get_num_states, get_state_hash
 import numpy as np
 from Framework.Agent import Agent
-#TODO: tf.keras....
-from tensorflow.keras.losses import Huber
-from keras.models import clone_model
-from keras.utils import to_categorical
-from keras.optimizers import Adam
-import keras.backend as K
+from Framework.utils import get_num_states
+import seaborn as sns
 
-# Hallo Max
+import matplotlib.pyplot as plt
+
 
 class DQN(Agent):
-    def __init__(self, environment, configuration, name='', verbosity_level = 10, debug=False):
+    def __init__(self, environment, configuration, name='', debug=False):
+        # init super
+        Agent.__init__(self, environment=environment, debug=debug, name=name, configuration=configuration)
+
+
+
         self.D = []  # Memory
         self.num_states = get_num_states(environment)
         self.model = configuration.model
-        self.lr = configuration.training_params.pop(0)
-        self.lr_cooling = configuration.cooling_scheme.pop(0)
-        self.model.compile(loss=Huber(), optimizer=Adam(self.lr)) #TODO : optimizer in Configuration
-        self.target_model = clone_model(self.model)
+        self.target_model = configuration.clone_model(self.model)
         self.memory_size = configuration.memory_size
 
         self.train_counter = 0
+        self.episode = 0
 
         self.init_memory(environment)
 
-        # init super lastly, because it starts the training
-        super().__init__(environment=environment, verbosity_level=verbosity_level, debug=debug, name=name,
-                         configuration=configuration)
+
 
     def init_memory(self, environment):
         while len(self.D) < self.memory_size:
@@ -49,15 +47,28 @@ class DQN(Agent):
             done = False
             while not done:
                 a = environment.action_space.sample()
-                s1, r, done = environment.step(a)
-                self.D.append((to_categorical(s, num_classes=self.num_states), a, r,
-                               to_categorical(s1, num_classes=self.num_states), done))
+                s1, r, done, _ = environment.step(a)
+                self.D.append((self.configuration.embedding(s), a, r,
+                               self.configuration.embedding(s1), done))
                 s = s1
                 if len(self.D) == self.memory_size:
                     break
 
-    def loss_function(self, theta, t):
-        return
+    def save(self, path):
+        if path == '' :
+            raise FileNotFoundError('Please provide a path')
+        else:
+            self.model.save_weights(path)
+
+
+    def load(self, path):
+        if path == '':
+            raise FileNotFoundError('Please provide a path to the model.')
+        elif not self.model:
+            raise AssertionError('A model must be specified to load the weights')
+        else:
+            self.model.load_weights(path)
+
 
     def train1(self, train_params, batch_size, verb=True):
         gamma, epsilon = train_params
@@ -65,35 +76,33 @@ class DQN(Agent):
         state = self.environment.reset()
         total_reward = 0
 
+        self.episode += 1
+
         # sample from policy
         done = False
+        batch_losses = []
         while not done:
             a = self.sample_action(state, [epsilon])
-            new_state, r, done, = self.environment.step(a)
+            new_state, r, done,_ = self.environment.step(a)
             total_reward += r
             self.D.pop(0)
-            self.D.append((to_categorical(state, num_classes=self.num_states), a, r,
-                           to_categorical(new_state, num_classes=self.num_states), done))
+            self.D.append((self.configuration.embedding(state), a, r,
+                           self.configuration.embedding(new_state), done))
 
             # create minibatch for training
             # for _ in range(1):
             mB_ind = np.random.choice(range(self.memory_size), size=batch_size, replace=True)
             mB = np.array(self.D)[mB_ind]
-            y = np.zeros((batch_size, self.environment.action_space.n))
-            x = np.zeros((batch_size, self.num_states))
-            batch_losses = []
+            x = np.concatenate(mB[:, 0]).reshape(batch_size, self.num_states)
+            y = self.model.predict(x)
+            y_target = gamma * np.max(self.target_model.predict(np.concatenate(mB[:, 3]).reshape(batch_size, self.num_states)), axis=1)
+
             for j in range(batch_size):
-                x[j] = mB[j][0]
-                y[j] = self.model.predict(x[j].reshape(1, self.num_states))[0]
-                y[j][mB[j][1]] = mB[j][2] * mB[j][-1] or mB[j][2] + gamma * max(
-                    self.target_model.predict(mB[j][3].reshape(1, self.num_states))[0])
+                y[j][mB[j][1]] = mB[j][2] * mB[j][-1] or mB[j][2] + y_target[j]
 
             # train
             batch_loss = self.model.train_on_batch(x, y)
             batch_losses.append(batch_loss)
-
-            if verb:
-                print('train it:', (self.train_counter+1), ' Return: ', total_reward, ' Loss: ', np.mean(batch_losses))
 
             # replace target model
             if self.train_counter % self.configuration.target_replacement == 0:
@@ -104,11 +113,32 @@ class DQN(Agent):
             # update state
             state = new_state
 
+        if verb:
+            print('train it:', self.episode, ' Return: ', total_reward, ' Loss: ', np.mean(batch_losses))
+
         return total_reward
+
+    def plot_q_values(self, states, shape = (4,3), iteration = 0, camera=None):
+        maxi = 0
+        matrix = np.zeros((np.prod(shape), np.prod(shape)))
+        for i in range(states):
+            j, k = int(i / shape[0]), i % shape[0]
+            q_val = self.model.predict(self.configuration.embedding(i)[np.newaxis,:])[0]
+            if np.max(q_val) > maxi:
+                maxi = np.max(q_val)
+            matrix[1 + (shape[1] * j), 0 + (shape[1] * k)] = q_val[0]  # left
+            matrix[2 + (shape[1] * j), 1 + (shape[1] * k)] = q_val[1]  # down
+            matrix[1 + (shape[1] * j), 2 + (shape[1] * k)] = q_val[2]  # right
+            matrix[0 + (shape[1] * j), 1 + (shape[1] * k)] = q_val[3]  # up
+
+        ax = sns.heatmap(matrix, vmin=0, vmax=maxi, cmap='YlGnBu')  # "RdBu_r")
+        ax.hlines([3, 6, 9], *ax.get_xlim())
+        ax.vlines([3, 6, 9], *ax.get_ylim())
+        plt.show()
 
     def get_action(self, state):
         return np.argmax(
-            self.model.predict(to_categorical(state, num_classes=self.num_states).reshape(1, self.num_states)))
+            self.model.predict(self.configuration.embedding(state)[np.newaxis,:]))
 
     def sample_action(self, state, params):
         epsilon = params[0]
